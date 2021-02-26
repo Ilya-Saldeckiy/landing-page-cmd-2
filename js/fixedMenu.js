4ad115a025465761d9ad5e291f59e8c7ee3832fc
Илья <67915032+Ilya-Saldeckiy@users.noreply.github.com>
2021-02-27 00:11:15 +0300

    Add files via upload

--- js/fixedMenu.js
+++ js/fixedMenu.js
@@ -1,14 +1,77 @@
-var scrolled;
-window.onscroll = function() {
-    scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if(scrolled > 500){
-        $(".header-section-fixed").addClass("fixed-active")
-    }
-    if(500 > scrolled){
-        $(".header-section-fixed").removeClass("fixed-active")         
-    }
-    if(scrolled > 7380){
-        $(".header-section-fixed").removeClass("fixed-active")         
-    }
+// var scrolled;
+// window.onscroll = function() {
+//     scrolled = window.pageYOffset || document.documentElement.scrollTop;
+//     if(scrolled > 500){
+//         $(".header-section-fixed").addClass("fixed-active")
+//     }
+//     if(500 > scrolled){
+//         $(".header-section-fixed").removeClass("fixed-active")         
+//     }
+//     if(scrolled > 7380){
+//         $(".header-section-fixed").removeClass("fixed-active")         
+//     }
+// }
 
+if(document.documentElement.clientWidth > 1600){
+    var scrolled;
+    window.onscroll = function() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled > 500){
+            $(".header-section-fixed").addClass("fixed-active")
+        }
+        if(500 > scrolled){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+        if(scrolled > 7400){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+    }
+}
+
+if(document.documentElement.clientWidth > 1400){
+    var scrolled;
+    window.onscroll = function() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled > 500){
+            $(".header-section-fixed").addClass("fixed-active")
+        }
+        if(500 > scrolled){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+        if(scrolled > 7500){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+    }
+}
+
+if(document.documentElement.clientWidth > 1000){
+    var scrolled;
+    window.onscroll = function() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled > 500){
+            $(".header-section-fixed").addClass("fixed-active")
+        }
+        if(500 > scrolled){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+        if(scrolled > 8050){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+    }
+}
+
+if(document.documentElement.clientWidth > 320){
+    var scrolled;
+    window.onscroll = function() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled > 500){
+            $(".header-section-fixed").addClass("fixed-active")
+        }
+        if(500 > scrolled){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+        if(scrolled > 10000){
+            $(".header-section-fixed").removeClass("fixed-active")         
+        }
+    }
 }
